5f233f5f721e29e0a1ff95dc9bb230c1375e45df
kysman2580 <165656455+kysman2580@users.noreply.github.com>
2025-09-30 15:16:17 +0900

    Add liveF_eurekaServer module and configure service dependencies

---
+++ liveF_eurekaServer/src/test/java/org/example/livef_eurekaserver/LiveFEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.livef_eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveFEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
